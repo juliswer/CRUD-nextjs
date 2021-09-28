dc148d3fa61eee1960ccbb053320ee1a298bf4a4
juliswer <julianswer36@gmail.com>
2021-09-28 16:05:52 -0300

    uploading context

--- src/context/taskContext.js
+++ src/context/taskContext.js
@@ -6,6 +6,8 @@ export const useTasks = () => useContext(TaskContext);
 
 export const TasksProvider = ({children}) => {
     const [tasks, setTasks] = useState([{id: '1', title: 'some title', description: 'some description'}])
-
-    return <TaskContext.Provider value={{tasks}}>{children}</TaskContext.Provider>
+    const createTask = (title, description) => {
+        setTasks([...tasks, {title, description, id: "2"}])
+    }
+    return <TaskContext.Provider value={{tasks, createTask}}>{children}</TaskContext.Provider>
 }
--- src/pages/new.js
+++ src/pages/new.js
@@ -1,12 +1,51 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { useState } from "react";
+import {useTasks} from '../context/taskContext';
 
 const TaskFormPage = () => {
+  const [task, setTask] = useState({
+      title: '',
+      description: '',
+  });
+
+  const {createTask} = useTasks();
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setTask({... task, [name]: value})
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask(task.title, task.description)
+  }
+
   return (
     <Layout>
-      <h1>Add a Task</h1>
-      <input type="text" placeholder="Write a title" className="bg-gray-400 focus:text-gray-100 focus:outline-none" />
-      <textarea rows="2" placeholder="Write a description"></textarea>
+      <form onSubmit={handleSubmit}>
+        <h1>Add a Task</h1>
+        <input
+          type="text"
+          name="title"
+          onChange={handleChange}
+          placeholder="Write a title"
+          className="bg-gray-800 focus:text-gray-100 focus:outline-none w-full py-3 px-4 mb-5"
+        />
+        <textarea
+          rows="2"
+          onChange={handleChange}
+          name="description"
+          placeholder="Write a description"
+          className="bg-gray-800 focus:text-gray-100 focus:outline-none w-full py-3 px-4 mb-5"
+        ></textarea>
+        <button
+          className="bg-green-500 hover:bg-green-400 px-4 py-2 rounded-sm disabled:opacity-30"
+          disabled={!task.title}
+        >
+          Save
+        </button>
+      </form>
     </Layout>
   );
 };
