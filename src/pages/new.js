9667f2f0ea502decb332af8f4d24f0dea3540fa0
juliswer <julianswer36@gmail.com>
2021-09-28 15:39:04 -0300

    uploading context

--- src/pages/new.js
+++ src/pages/new.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Layout from '../components/Layout';
+import React from "react";
+import Layout from "../components/Layout";
 
 const TaskFormPage = () => {
-    return (
-        <Layout>
-            <h1>Add a Task</h1>
-            <input type="text" placeholder="Write a title"/>
-            <textarea rows="2" placeholder="Write a description"></textarea>
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <h1>Add a Task</h1>
+      <input type="text" placeholder="Write a title" className="bg-gray-400 focus:text-gray-100 focus:outline-none" />
+      <textarea rows="2" placeholder="Write a description"></textarea>
+    </Layout>
+  );
+};
 
-export default TaskFormPage
+export default TaskFormPage;
