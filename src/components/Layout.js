29392e56a7871a0229965fc8506afcd67e83ac39
juliswer <julianswer36@gmail.com>
2021-09-28 15:24:46 -0300

    uploading context

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import {AiOutlinePlus} from 'react-icons/ai';
+import Link from 'next/link';
+import {useRouter} from 'next/router';
 
 const Layout = ({children}) => {
+
+    const router = useRouter();
+
     return (
-        <div>
-            <header>
-                <h1>Task App</h1>
-                <div>
-                    <button>Add task</button>
+        <div className="h-screen bg-gray-900 text-white">
+            <header className="flex bg-gray-800 px-28 py-5">
+                <Link href="/">
+                <a>
+                    <h1 className="font-black text-lg">Task App</h1>    
+                </a>
+                </Link>
+                <div className="flex-grow text-right">
+                    <button className="bg-green-500 hover:bg-green-400 py-2 px-5 font-bold rounded-sm inline-flex items-center" onClick={() => router.push('/new')}><AiOutlinePlus className="mr-2" /> Add task</button>
                 </div>
             </header>
-            <hr />
-            {children}
+            <main className="px-28 ">
+                {children}
+            </main>
         </div>
     )
 }
