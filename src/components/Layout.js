ec450b6c50d9408895fdcde0c54d5ebfa0db7706
juliswer <julianswer36@gmail.com>
2021-09-28 15:33:36 -0300

    uploading the form files

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,12 @@ import React from 'react'
 import {AiOutlinePlus} from 'react-icons/ai';
 import Link from 'next/link';
 import {useRouter} from 'next/router';
+import {useTasks} from '../context/taskContext';
 
 const Layout = ({children}) => {
 
     const router = useRouter();
+    const {tasks}= useTasks()
 
     return (
         <div className="h-screen bg-gray-900 text-white">
@@ -15,7 +17,7 @@ const Layout = ({children}) => {
                     <h1 className="font-black text-lg">Task App</h1>    
                 </a>
                 </Link>
-                <span className="ml-2 text-gray-400 font-bold">10 tareas</span>
+                <span className="ml-2 text-gray-400 font-bold">{tasks.length} Tasks</span>
                 <div className="flex-grow text-right">
                     <button className="bg-green-500 hover:bg-green-400 py-2 px-5 font-bold rounded-sm inline-flex items-center" onClick={() => router.push('/new')}><AiOutlinePlus className="mr-2" /> Add task</button>
                 </div>
--- src/pages/new.js
+++ src/pages/new.js
@@ -4,9 +4,9 @@ import Layout from '../components/Layout';
 const TaskFormPage = () => {
     return (
         <Layout>
-            <div> 
-                New Task 
-            </div>
+            <h1>Add a Task</h1>
+            <input type="text" placeholder="Write a title"/>
+            <textarea rows="2" placeholder="Write a description"></textarea>
         </Layout>
     )
 }
