64dd81b5651228bce808d20d0808ac672784bc66
juliswer <julianswer36@gmail.com>
2021-09-28 16:52:45 -0300

    uploading context

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,53 @@
-import React from 'react'
-import {useTasks} from '../context/taskContext';
-import {useContext} from 'react';
-import Layout from '../components/Layout';
+import React from "react";
+import { useTasks } from "../context/taskContext";
+import Layout from "../components/Layout";
+import { useRouter } from "next/router";
+import { VscTrash, VscTasklist } from "react-icons/vsc";
 
-const Home = () => {
-
-  const {tasks} = useTasks();
-
-  console.log(tasks);
+function Home() {
+  const { tasks, deleteTask } = useTasks();
+  const router = useRouter();
 
   return (
     <Layout>
-      <div>Hello world</div>
+      <div className="flex justify-center">
+        {tasks.length === 0 ? (
+          <div className="block">
+            <h2 className="text-2xl">There are no tasks</h2>
+            <VscTasklist size="8rem" />
+          </div>
+        ) : (
+          <div className="w-7/10">
+            {tasks.map((task, i) => (
+              <div
+                key={task.id}
+                className="bg-gray-700 hover:bg-gray-600 cursor-pointer px-20 py-5 m-2 flex justify-between"
+                onClick={() => router.push("/edit/" + task.id)}
+              >
+                <span className="text-5xl mr-5">{i}</span>
+                <div>
+                  <div className="flex justify-between">
+                    <h1 className="font-bold">{task.title}</h1>
+                    <button
+                      className="bg-red-700 hover:bg-red-600 px-3 py-1 inline-flex items-center"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        deleteTask(task.id);
+                      }}
+                    >
+                      <VscTrash className="mr-2" /> Delete
+                    </button>
+                  </div>
+                  <p className="text-gray-300">{task.description}</p>
+                  <span className="text-gray-400">{task.id}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </Layout>
-  )
+  );
 }
 
-export default Home
+export default Home;
